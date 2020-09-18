71fc503e1e18d4baf915768d8b8c0b48b28aedd1
WUSO01 <hi.wuso01@gmail.com>
2020-09-18 16:14:12 +0800

    Revert "fix: build site的时候没有编译package导致打包不进去"
    This reverts commit 8db8035a742731094e575b337c4ecc630f508c70.

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,14 @@
 import { terser } from 'rollup-plugin-terser'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
+import resolve from '@rollup/plugin-node-resolve'
 import copy from 'rollup-plugin-copy'
-import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'demos/index.js',
   output: {
-    format: 'umd',
     file: '_site/index.js',
   },
-  onwarn: function (warning) {
-    if (warning.code === 'THIS_IS_UNDEFINED') {
-      return
-    }
-  },
   plugins: [
-    nodeResolve(),
-    commonjs(),
+    resolve(),
     terser(),
     copy({
       targets: [
