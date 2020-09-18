7ebaeaf448646601cb2b38e03865a0fd4486f269
WUSO01 <hi.wuso01@gmail.com>
2020-09-18 16:42:14 +0800

    fix: yml syntax error

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,22 @@
 import { terser } from 'rollup-plugin-terser'
-import resolve from '@rollup/plugin-node-resolve'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import copy from 'rollup-plugin-copy'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'demos/index.js',
   output: {
+    format: 'umd',
     file: '_site/index.js',
   },
+  onwarn: function (warning) {
+    if (warning.code === 'THIS_IS_UNDEFINED') {
+      return
+    }
+  },
   plugins: [
-    resolve(),
+    nodeResolve(),
+    commonjs(),
     terser(),
     copy({
       targets: [
