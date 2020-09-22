24a64317882941d43c6dae5049408f5a5350f2f7
WUSO01 <hi.wuso01@gmail.com>
2020-09-22 21:17:04 +0800

    chore: 📦 引入storybook

---
+++ packages/belle-button/stories/button.stories.ts
@@ -0,0 +1,40 @@
+import { TemplateResult, html } from 'lit-element'
+
+import '@belleui/belle-button'
+
+export default {
+  title: 'Button',
+  component: 'belle-button'
+}
+
+export const Default = (): TemplateResult => html`
+  <belle-button>Default</belle-button>
+  <belle-button type="primary">Primary</belle-button>
+  <belle-button type="success">Success</belle-button>
+  <belle-button type="danger">Danger</belle-button>
+  <belle-button type="warning">Warning</belle-button>
+  <belle-button type="dark">Dark</belle-button>
+  <belle-button type="light">Light</belle-button>
+`
+
+Default.story = {
+  name: 'Default'
+}
+
+export const outline = (): TemplateResult => html`
+  <belle-button outline>Default</belle-button>
+  <belle-button type="primary" outline>Primary</belle-button>
+  <belle-button type="success" outline>Success</belle-button>
+`
+
+outline.story = {
+  name: 'Outline'
+}
+
+export const disable = (): TemplateResult => html`
+  <belle-button type="primary" disabled>Primary</belle-button>
+`
+
+disable.story = {
+  name: 'Disable'
+}
---
+++ packages/belle-radio/stories/radio.stories.ts
@@ -0,0 +1,16 @@
+import { TemplateResult, html } from 'lit-element'
+
+import '@belleui/belle-radio'
+
+export default {
+  title: 'Radio',
+  component: 'belle-radio'
+}
+
+export const Default = (): TemplateResult => html`
+  <belle-radio>Radio</belle-radio>
+`
+
+Default.story = {
+  name: 'Default'
+}
