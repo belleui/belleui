06f8af1ea5ec40d499e969434bb6036d3df2df05
2013WUSO01 <hi.wuso01@gmail.com>
2020-09-25 11:28:59 +0800

    chore: 新增快速创建组件命令 (#22)
    create:component

---
+++ scripts/create/index.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk')
+const dedent = require('dedent')
+
+console.log(
+  chalk.cyan(dedent`
+    🙏 感谢您的贡献
+
+    🚀 当前命令用于快速创建组件
+
+    💡 组件名应该添加'belle'前缀, 如：belle-button
+  `)
+)
