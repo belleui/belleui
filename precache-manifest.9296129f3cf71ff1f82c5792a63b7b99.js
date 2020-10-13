8d133ec8966d51926faad08cbfa51e20d9a49f18
WUSO01 <WUSO01@users.noreply.github.com>
2020-10-13 10:14:09 +0000

    deploy: 2a9f50fa96ccbf0dd7580e20a85fa451ff3e7fc3

---
+++ precache-manifest.9296129f3cf71ff1f82c5792a63b7b99.js
@@ -0,0 +1,28 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "1:7447cc88d532956491f1.js"
+  },
+  {
+    "url": "1:7447cc88d532956491f1.js.map"
+  },
+  {
+    "revision": "82ad446fb725b23bf9aea94c8e457eaa",
+    "url": "images/screenshot.bed5792.png"
+  },
+  {
+    "revision": "2a033906d5a2a6211b6a",
+    "url": "index.7447cc8.bundle.js"
+  },
+  {
+    "revision": "5a73259064fe15d3f24e8a1f8345efaf",
+    "url": "index.7447cc8.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a033906d5a2a6211b6a",
+    "url": "index.7447cc8.bundle.js.map"
+  },
+  {
+    "revision": "fca9c041735480eed1cecaefb0d715ba",
+    "url": "index.html"
+  }
+]);
